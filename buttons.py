davidrwilliamson/fish-annotator

commit 621b27103008de01b1690466dd3b953a90047936

Changing canvas sizes based on image size now seems to work correctly. RoIs and painted annotations are still displayed with an incorrect y offset, even though they are exported correctly. NN preview shows in the right place, though.
Fixed a couple of bugs related to loading and saving annotations.

main_window.py
@@ -141,13 +141,14 @@ class MainWindow(QMainWindow):
         save: bool = False
         i = 0
         for canvas in self.annotation_canvases:
-            if canvas.is_used:  # Don't bother to save unless something has actually been drawn
+            if canvas.is_used or canvas.is_cleared:  # Don't bother to save unless something has actually been drawn
                 buffer = QBuffer()
                 buffer.open(QIODevice.ReadWrite)
                 canvas.pixmap().save(buffer, 'png')
                 buffer.close()
                 canvases[i] = buffer
                 canvas.erase_all()
+                canvas.is_cleared = False
                 save = True
             i += 1
         if save:
@@ -200,8 +201,13 @@ class MainWindow(QMainWindow):
         if os.path.isdir(save_folder):
             annotations = [file for file in os.listdir(save_folder) if os.path.splitext(file)[1] == '.png']
             for file in annotations:
-                # TODO: This fails with the new names silc_bayer and bayer_silc. Fix comprehension
-                i, j = list(map(int, os.path.splitext(file)[0].split('_')[1:]))
+                underscore_split = os.path.splitext(file)[0].split('_')
+                if len(underscore_split) == 4:
+                    i, j = list(map(int, os.path.splitext(file)[0].split('_')[2:]))
+                elif len(underscore_split) == 3:
+                    i, j = list(map(int, os.path.splitext(file)[0].split('_')[1:]))
+                else:
+                    raise
                 full_path = os.path.join(save_folder, file)
 
                 pmap = QPixmap()
@@ -220,6 +226,13 @@ class MainWindow(QMainWindow):
                         self.saved_canvases[i].append(None)
                 self.saved_canvases[i][j] = buffer
 
+    def adjust_frame_sizes(self, im_folder: ImageFolder) -> None:
+        w, h = im_folder.frame_w, im_folder.frame_h
+        for canvas in [self.image_frame, self.rois_canvas, self.nn_preview_canvas]:
+            canvas.set_frame_size(w, h)
+        for canvas in self.annotation_canvases:
+            canvas.set_frame_size(w, h)
+
     @pyqtSlot()
     def save_ann(self):
         self.save_annotations_mem()
@@ -234,7 +247,7 @@ class MainWindow(QMainWindow):
         self.save_annotations_disk()
 
         self.im_folder = im_folder
-        self.image_frame.adjust_frame_size(im_folder.frame_w / 2, im_folder.frame_h / 2)
+        self.adjust_frame_sizes(im_folder)
         self.saved_canvases = [None for i in range(im_folder.num_frames)]
         self.load_annotations_disk()
         self.load_annotations_mem()
@@ -272,6 +285,11 @@ class MainWindow(QMainWindow):
             self.right_buttons.btn_erase.setChecked(True)
         if idx == ToolBtn.CLEAR:
             ann_layer.erase_all()
+            # Had a bug where if a canvas was cleared and then we changed layers without drawing anything
+            # it would be marked as unused and not saved.
+            # Now we check for is_cleared when saving too, and set it to false after a save.
+            # TODO: We do end up saving blank annotations in this case, though. Should fix.
+            ann_layer.is_cleared = True
             ann_layer.update()
             self.change_tool(ToolBtn.PAINT)
         if idx == ToolBtn.REVERT:

canvases.py
@@ -9,15 +9,19 @@ import cv2 as cv
 class MainCanvas(QLabel):
     def __init__(self, parent: QWidget) -> None:
         super(MainCanvas, self).__init__(parent)
-        self._w, self._h = 1224, 425
-        self.setMinimumSize(QSize(self._w, self._h))
-        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        self._set_canvas()
+        self._w, self._h = 0, 0
+        # self.set_frame_size(self._w, self._h)
 
     def _set_canvas(self) -> None:
         canvas = QPixmap(self._w, self._h)
         canvas.fill(QColor('transparent'))
         self.setPixmap(canvas)
+
+    def set_frame_size(self, w, h) -> None:
+        self._w, self._h = w / 2, h / 2
+        self.setMinimumSize(QSize(self._w, self._h))
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self._set_canvas()
 
 
 class RoIsCanvas(MainCanvas):
@@ -61,6 +65,7 @@ class PaintingCanvas(MainCanvas):
         self.pen_colour = QColor(colour)
         self.pen_size = 3
         self.is_used = False
+        self.is_cleared = False
         self.brush_erase = False
 
     def mouseMoveEvent(self, e: QMouseEvent) -> None:
@@ -138,10 +143,6 @@ class ImageFrame(MainCanvas):
         elif self.image is not None:
             painter.drawPixmap(0, 0, self.image)
         painter.end()
-
-    def adjust_frame_size(self, w, h) -> None:
-        self._w, self._h = w, h
-        self._set_canvas()
 
     def _adjust_image(self) -> None:
         im: QImage = self.image.toImage()
@@ -261,20 +262,21 @@ class NNPreviewCanvas(MainCanvas):
         return points
 
     def paintEvent(self, event) -> None:
-        painter = QPainter()
-        painter.begin(self)
+        if self._w > 0 and self._h > 0:
+            painter = QPainter()
+            painter.begin(self)
 
-        p = painter.pen()
-        p.setWidth(self.pen_size)
-        p.setColor(self.pen_color)
-        painter.setPen(p)
-        painter.drawPixmap(0, 0, self.pixmap())
+            p = painter.pen()
+            p.setWidth(self.pen_size)
+            p.setColor(self.pen_color)
+            painter.setPen(p)
+            painter.drawPixmap(0, 0, self.pixmap())
 
-        if self.points:
-            for group in self.points:
-                for point in group:
-                    p = point[0]
-                    p = QPoint(p[0], p[1])
-                    painter.drawPoint(p)
+            if self.points:
+                for group in self.points:
+                    for point in group:
+                        p = point[0]
+                        p = QPoint(p[0], p[1])
+                        painter.drawPoint(p)
 
-        painter.end()
+            painter.end()

menus.py
@@ -71,9 +71,9 @@ class MainMenu(QMenuBar):
 
         # action_preview_rois = QAction('&Preview RoIs', self)
         # action_preview_rois.triggered.connect(lambda: self._call_export(ExportMenu.PREVIEW_ROIS))
-        action_export_rois = QAction('Export &RoIs', self)
+        action_export_rois = QAction('Export cropped &RoIs', self)
         action_export_rois.triggered.connect(lambda: self._call_export(ExportMenu.EXPORT_ROIS))
-        action_export_full = QAction('Export &full frames', self)
+        action_export_full = QAction('Export all &full frames', self)
         action_export_full.triggered.connect(lambda: self._call_export(ExportMenu.EXPORT_FULL))
         action_export_interesting = QAction('Export only &interesting frames', self)
         action_export_interesting.triggered.connect(lambda: self._call_export(ExportMenu.EXPORT_INTERESTING))
@@ -83,9 +83,9 @@ class MainMenu(QMenuBar):
         action_export_current.triggered.connect(lambda: self._call_export(ExportMenu.EXPORT_CURRENT))
 
         # export_menu.addAction(action_preview_rois)
-        self.export_menu.addAction(action_export_rois)
         self.export_menu.addAction(action_export_full)
         self.export_menu.addAction(action_export_interesting)
+        self.export_menu.addAction(action_export_rois)
         self.export_menu.addAction(action_export_montage)
         self.export_menu.addAction(action_export_current)
 
@@ -184,12 +184,14 @@ class MainMenu(QMenuBar):
         try:
             os.mkdir(root_path)
         except FileExistsError:
-            print('Directory {} already exists.'.format(os.path.join(root_path)))
+            pass
+            # print('Directory {} already exists.'.format(os.path.join(root_path)))
 
         try:
             os.mkdir(subfolder)
         except FileExistsError:
-            print('Directory {} already exists.'.format(subfolder))
+            pass
+            # print('Directory {} already exists.'.format(subfolder))
 
         return subfolder
 
@@ -204,6 +206,7 @@ class MainMenu(QMenuBar):
 
             save_path = os.path.join(save_folder, '{}_cropped.png'.format(im_folder.framepos[1]))
             im_save.save(save_path, 'png')
+        print('Saved images to: {}'.format(save_folder))
 
     def export_full_frames(self, im_folder: ImageFolder) -> None:
         save_folder = self._make_export_folder(im_folder.folder, ExportMenu.EXPORT_FULL)
@@ -215,6 +218,7 @@ class MainMenu(QMenuBar):
 
             save_path = os.path.join(save_folder, '{}_full.png'.format(im_folder.framepos[1]))
             im_raw.save(save_path, 'png')
+        print('Saved images to: {}'.format(save_folder))
 
     def export_montage(self, im_folder: ImageFolder) -> None:
         pass
@@ -230,8 +234,10 @@ class MainMenu(QMenuBar):
 
             save_path = os.path.join(save_folder, '{}_full.png'.format(im_folder.framepos[1]))
             im_raw.save(save_path, 'png')
+        print('Saved images to: {}'.format(save_folder))
 
-    def export_current(self, im_folder: ImageFolder, image_frame: 'ImageFrame', rois_canvas, painting_canvas, nn_preview_canvas) -> None:
+    def export_current(self, im_folder: ImageFolder, image_frame: 'ImageFrame',
+                       rois_canvas, painting_canvas, nn_preview_canvas) -> None:
         save_folder = self._make_export_folder(im_folder.folder, ExportMenu.EXPORT_CURRENT)
         n = 0
         save_path = os.path.join(save_folder, '{}_selected_view_{}.png'.format(im_folder.framepos[1], n))
@@ -245,14 +251,15 @@ class MainMenu(QMenuBar):
             base_im: QImage = image_frame.image.toImage()
 
         im_size = base_im.size()
-        im = base_im.copy()
+        im: QImage = base_im.copy()
         im = im.convertToFormat(QImage.Format_ARGB32)
 
         painter = QPainter(im)
         painter.setCompositionMode(painter.CompositionMode_SourceOver)
         for canvas in [rois_canvas, painting_canvas, nn_preview_canvas]:
             if canvas is not None:
-                canvas_image = canvas.pixmap().toImage()
+                canvas_image: QImage = canvas.pixmap().toImage()
                 painter.drawImage(QRect(QPoint(0, 0), im_size), canvas_image)
         painter.end()
         im.save(save_path, 'png')
+        print('Saved image: {}'.format(save_path))

commit 01016e40b5cc4f280056307e2abdddb12606a7ee

Started implementing ability to "zoom" images to full size. Right now this interferes with annotations - they get saved at one of two different sizes and don't display properly after changing to a different frame/zoom and coming back again. Should save all annotations at full image size and investigate other problem. Changing zoom also messes up the button layout.
Also removed bug introduced in 621b2710 where annotations were being marked cleared and therefore not saved. Now the bug that was meant to fix is back in.

buttons.py
@@ -30,15 +30,26 @@ class BottomButtons(QWidget):
     sgnl_adjust_brightness = pyqtSignal(int)
     sgnl_adjust_contrast = pyqtSignal(int)
     sgnl_toggle_scale_bar = pyqtSignal(bool)
+    sgnl_toggle_zoom = pyqtSignal(bool)
 
     def __init__(self, parent: QWidget = None) -> None:
         super(BottomButtons, self).__init__(parent)
         bb_layout = QVBoxLayout(self)
 
+        cb_layout = QHBoxLayout()
+
         self.cb_scale_bar = QCheckBox('Show scale bar')
-        bb_layout.addWidget(self.cb_scale_bar, alignment=Qt.AlignRight)
+        cb_layout.addWidget(self.cb_scale_bar, alignment=Qt.AlignRight)
         self.cb_scale_bar.toggled.connect(self._call_toggle_scale_bar)
         self.cb_scale_bar.setChecked(False)
+
+        self.cb_zoom = QCheckBox('2x zoom')
+        cb_layout.addWidget(self.cb_zoom, alignment=Qt.AlignRight)
+        self.cb_zoom.toggled.connect(self._call_toggle_zoom)
+        self.cb_zoom.setChecked(False)
+        self.cb_zoom.setEnabled(False)
+
+        bb_layout.addLayout(cb_layout)
 
         self.btn_start = QPushButton('Start')
         self.btn_end = QPushButton('End')
@@ -91,10 +102,13 @@ class BottomButtons(QWidget):
     def _call_toggle_scale_bar(self, checked: bool) -> None:
         self.sgnl_toggle_scale_bar.emit(checked)
 
+    def _call_toggle_zoom(self, checked: bool) -> None:
+        self.sgnl_toggle_zoom.emit(checked)
+
     def enable_buttons(self, enable: bool = True) -> None:
         """Sets all buttons to enabled (by default) or disable (if passed False as argument)."""
         buttons = [self.btn_start, self.btn_end, self.btn_next, self.btn_prev,
-                   self._sldr_brightness, self._sldr_contrast]
+                   self._sldr_brightness, self._sldr_contrast, self.cb_zoom]
         for btn in buttons:
             btn.setEnabled(enable)
 

main_window.py
@@ -82,6 +82,7 @@ class MainWindow(QMainWindow):
         self.bottom_buttons.sgnl_adjust_brightness.connect(self.adjust_brightness)
         self.bottom_buttons.sgnl_adjust_contrast.connect(self.adjust_contrast)
         self.bottom_buttons.sgnl_toggle_scale_bar.connect(self.toggle_scale_bar)
+        self.bottom_buttons.sgnl_toggle_zoom.connect(self.toggle_zoom)
         self.br_buttons.sgnl_toggle_bad_frame.connect(self.toggle_bad_frame)
         self.br_buttons.sgnl_toggle_interesting_frame.connect(self.toggle_interesting_frame)
         self.left_buttons.sgnl_toggle_bad_frames.connect(self.show_bad_frames)
@@ -289,7 +290,9 @@ class MainWindow(QMainWindow):
             # it would be marked as unused and not saved.
             # Now we check for is_cleared when saving too, and set it to false after a save.
             # TODO: We do end up saving blank annotations in this case, though. Should fix.
-            ann_layer.is_cleared = True
+            # ann_layer.is_cleared = True
+            # My "fix" ended up introducing a new bug where annotations are not saved, because we call the clear tool in a few other places.
+            # I guess we need a way to know that a previously not empty annotation has been made empty.
             ann_layer.update()
             self.change_tool(ToolBtn.PAINT)
         if idx == ToolBtn.REVERT:
@@ -410,6 +413,18 @@ class MainWindow(QMainWindow):
             self.scale_bar.setVisible(True)
         else:
             self.scale_bar.setVisible(False)
+
+    @pyqtSlot(bool)
+    def toggle_zoom(self, checked: bool) -> None:
+        canvases = [self.image_frame, self.rois_canvas, self.nn_preview_canvas]
+        canvases.extend(self.annotation_canvases)
+        for canvas in canvases:
+            w, h = self.im_folder.frame_w, self.im_folder.frame_h
+            if checked:
+                w *= 2
+                h *= 2
+            canvas.set_frame_size(w, h)
+        self.change_frame(NavBtn.NOCHANGE)
 
     @pyqtSlot(bool)
     def show_bad_frames(self, checked: bool) -> None: